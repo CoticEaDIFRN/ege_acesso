4e3361c64a9e90b1d67b73cf92f9f5d2a7d51a59
Luiz Antonio <luizvpc@gmail.com>
2019-08-06 12:47:12 -0300

    [UPD] - Changing user data in 'acesso'

--- ege_acesso/services.py
+++ ege_acesso/services.py
@@ -17,15 +17,22 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.views import APIView
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.routers import DefaultRouter
 from .models import User
 from .serializers import UserSerializer
+from django.http import HttpResponse, HttpResponseNotFound
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 class BaseModelService(ReadOnlyModelViewSet):
@@ -36,6 +43,34 @@ class UserService(BaseModelService):
     queryset = User.objects.filter(deleted__isnull=True)
     serializer_class = UserSerializer
 
+
+# Trocar isso para APIView
+class UserBiografyService(View):
+
+    def get(self, request, username, *args, **kwargs):
+        user = get_object_or_404(User, username=username)
+        return HttpResponse(json.dump({"biografy": user.biografy}))
+
+    @method_decorator(csrf_exempt)
+    def post(self, request, username, *args, **kwargs):
+        user = get_object_or_404(User, username=username)
+        user.biografy = request.POST['biografy']
+        user.save()
+        return HttpResponse('{"success": true}')
+
+
+class UserEmailService(View):
+
+    def get(self, request, username, *args, **kwargs):
+        user = get_object_or_404(User, username=username)
+        return HttpResponse(json.dump({"email": user.email}))
+
+    @method_decorator(csrf_exempt)
+    def post(self, request, username, *args, **kwargs):
+        user = get_object_or_404(User, username=username)
+        user.email = request.POST['email']
+        user.save()
+        return HttpResponse('{"success": true}')
 
 router = DefaultRouter()
 router.register('users', UserService)
--- ege_acesso/urls.py
+++ ege_acesso/urls.py
@@ -21,8 +21,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.views.generic.base import TemplateView
 from .views import authorize_view, validate_view, secret_validate_view
-from .services import router
+from .services import router, UserBiografyService, UserEmailService
 from django.views.generic import RedirectView
+from django.views.decorators.csrf import csrf_exempt
 
 
 app_name = 'ege_acesso'
@@ -32,6 +33,8 @@ urlpatterns = [
     path('', RedirectView.as_view(url="..")),
     path('', RedirectView.as_view(url=settings.URL_PATH_PREFIX)),
     path('api/v1/', include(router.urls)),
+    path('api/v1/users/<str:username>/biografy/', csrf_exempt(UserBiografyService.as_view())),
+    path('api/v1/users/<str:username>/email/', csrf_exempt(UserEmailService.as_view())),
     path('api/v1/secret/<str:secret>/', secret_validate_view),
     path('jwt/', TemplateView.as_view(template_name="ege_acesso/acesso_errado.html",
                                       extra_context={'perfil_url': settings.LOGIN_REDIRECT_URL})),
