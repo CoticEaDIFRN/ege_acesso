935939ad8d46785aaf1102420371bcb9ad719a3b
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2019-12-27 18:10:10 -0300

    [ADD] acesso specific attributes

--- settings.py
+++ settings.py
@@ -19,9 +19,41 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 import os
 from sc4py.env import env
-os.environ.setdefault("MY_APPS", "ege_acesso")
-os.environ.setdefault("DJANGO_AUTH_USER_MODEL", "ege_acesso.User")
-os.environ.setdefault("POSTGRES_DB", env("POSTGRES_DB_ACESSO"))
+URL_PATH_PREFIX = env("URL_PATH_PREFIX", "ege/acesso/")
 os.environ.setdefault("URL_PATH_PREFIX", env("", "ege/acesso/"))
+os.environ.setdefault("MY_APPS", "ege_acesso")
+os.environ.setdefault("POSTGRES_DB", env("POSTGRES_DB_ACESSO"))
+
+os.environ.setdefault("DJANGO_AUTH_USER_MODEL", "ege_acesso.User")
+os.environ.setdefault("DJANGO_LOGIN_URL", env("DJANGO_LOGIN_URL", '/%slogin/' % URL_PATH_PREFIX))
+os.environ.setdefault("DJANGO_LOGOUT_URL", env("DJANGO_LOGOUT_URL", '/%slogout/' % URL_PATH_PREFIX))
+os.environ.setdefault("DJANGO_LOGIN_REDIRECT_URL", env("DJANGO_LOGIN_REDIRECT_URL", '/%s' % URL_PATH_PREFIX))
+os.environ.setdefault("DJANGO_LOGOUT_REDIRECT_URL", env("DJANGO_LOGOUT_REDIRECT_URL", '/%s' % URL_PATH_PREFIX))
 
 from ege_utils.template_settings import *
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {'console': {'class': 'logging.StreamHandler'}, },
+    'loggers': {
+        '': {'handlers': ['console'], 'level': 'DEBUG'},
+        'parso': {'handlers': ['console'], 'level': 'INFO'},
+    },
+}
